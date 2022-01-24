21d74aa33654154c1a66c4f792fe5fdc274838f2
neimv <neimv.zatara@gmail.com>
2022-01-23 20:11:45 -0600

    testing server

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 
+import json as js
+
 import pandas as pd
-from flask import Flask
-from flask_restful import Resource, Api, abort
+import yaml as yml
+from dicttoxml import dicttoxml
+from flask import Flask, make_response
+from flask_restful import Resource, Api, abort, reqparse
 from sqlalchemy import create_engine
 
 
@@ -10,6 +14,9 @@ from sqlalchemy import create_engine
 #############################################################################
 app = Flask(__name__)
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('format')
 
 # @TODO, change this to good configs
 user = 'neimv'
@@ -26,15 +33,43 @@ class DataframesList(Resource):
     def get(self):
         try:
             df = pd.read_sql_table('all_dataframes', engine)
-            raise Exception('bye')
             df['dataframe_registers'] = df['dataframe_registers'].apply(
-                lambda x: x.replace('.', '_').replace(' ', '_').lower()
+                lambda x: x.lower()
             )
             response = df.to_dict(orient='records')
-        except:
-            abort(500)
+        except Exception as e:
+            abort(500, custom=f"ERROR: {e}")
+
+        return response
+
+
+class DataFrameGet(Resource):
+    def get(self, name: str):
+        args = parser.parse_args()
+        format_ = args.get('format', 'json')
+
+        try:
+            df = pd.read_sql_table(f'dataframe_{name}', engine)
+            df.fillna(0, inplace=True)
+            response = df.to_dict(orient='records')
+
+            if format_ == 'xml':
+                content_type = 'application/xml'
+                response = dicttoxml(response, custom_root=name)
+            elif format_ == 'json':
+                content_type = 'application/json'
+                response = js.dumps(response)
+            else:
+                content_type = 'application/json'
+                response = js.dumps(response)
+        except Exception as e:
+            abort(404, custom=f'ERROR: {e}')
+
+        response = make_response(response)
+        response.headers['content-type'] = content_type
 
         return response
 
 
 api.add_resource(DataframesList, '/')
+api.add_resource(DataFrameGet, '/<string:name>')
